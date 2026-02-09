9e3870693409b3465253df01bc5c3a0101603c67
Nikhil Tatikonda <93245730+Nikhil9490@users.noreply.github.com>
2026-02-08 21:22:05 -0500

    LC-valid-anagram [Runtime: 11 ms; Memory: 19.4 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,6 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return Counter(s) == Counter(t)
+        str1 = s.lower()
+        str2 = t.lower()
+
+        str1 = str1.replace(" ", "")
+        str2 = str2.replace(" ", "")
+
+
+        counts = [0] * 26
+
+        for char in str1:
+            counts[ord(char)-ord('a')]+= 1
+        for char in str2:
+            counts[ord(char)-ord('a')]-= 1
+
+        for count in counts:
+            if count!=0:
+                return False
+        return True
         
 
