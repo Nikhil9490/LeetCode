d6ca4a5efd5462295b1272c54775a99ed3926efb
Nikhil Tatikonda <93245730+Nikhil9490@users.noreply.github.com>
2026-02-10 00:00:41 -0500

    LC-top-k-frequent-elements [Runtime: 0 ms; Memory: 23.1 MB]

--- heap-priority-queue/top-k-frequent-elements.py
+++ heap-priority-queue/top-k-frequent-elements.py
@@ -1,16 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-            hashmap = defaultdict(int)
-            empty = []
-            for i in range(len(nums)):
-                hashmap[nums[i]]+=1
-            j=1
-            while j<=k:
-                max_key = max(hashmap, key= hashmap.get)
-                empty.append(max_key)
-                del hashmap[max_key]
-                j=j+1
+            counts = Counter(nums)
+            top_k = counts.most_common(k)
+            empty =[]
+            for num,freq in top_k:
+                empty.append(num)
             return empty
+            
 
 
 
