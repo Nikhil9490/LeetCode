478cf80dab109de57b7e8f25d2d3cdf35ec49eea
Nikhil Tatikonda <93245730+Nikhil9490@users.noreply.github.com>
2026-04-06 00:00:18 -0400

    LC-find-users-with-valid-e-mails [Runtime: N/A; Memory: N/A]

--- database/find-users-with-valid-e-mails.sql
+++ database/find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Users
-WHERE LOWER(mail) REGEXP '^[a-z][a-z0-9._-]*@leetcode\\.com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$';
